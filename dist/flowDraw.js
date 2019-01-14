155129f54e2e64f0be85cff6af0a85239e7b42dd
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-01-14 17:09:39 +0800

    Add a readme object

--- dist/flowDraw.js
+++ dist/flowDraw.js
@@ -810,9 +810,44 @@ var Cubeoid = Atom.create({
     }
 });
 
+var Readme = Atom.create({
+    codeBlock: "",
+    readmeText: "Readme text here",
+    type: "readme",
+    name: "README",
+    radius: 20,
+    updateSidebar: function(){
+        //updates the sidebar to display information about this node
+        
+        var valueList = DrawingNode.updateSidebar.call(this); //call the super function
+        
+        createEditableValueListItem(valueList,this,"readmeText", "Notes", false);
+        
+    },
+    draw: function() {
+        
+        DrawingNode.draw.call(this); //Super call to draw the rest
+        
+        //draw the two slashes on the node//
+        c.strokeStyle = "#949294";
+        c.lineWidth = 3;
+        c.lineCap = "round";
+        
+        c.beginPath();
+        c.moveTo(this.x - 11, this.y + 10);
+        c.lineTo(this.x, this.y - 10);
+        c.stroke();
+        
+        c.beginPath();
+        c.moveTo(this.x, this.y + 10);
+        c.lineTo(this.x + 11, this.y - 10);
+        c.stroke();
+    }
+});
+
 // Implementation
 
-var availableTypes = [Molecule, Sphereoid, Cubeoid, Input, Output, Constant];
+var availableTypes = [Molecule, Sphereoid, Cubeoid, Input, Output, Constant, Readme];
 
 let currentMolecule;
 let menu;
