2dbedf0b6561b647731918545122b62d93603a4b
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-03-10 11:00:32 +0800

    Working

--- dist/js/molecules/menu.js
+++ dist/js/molecules/menu.js
@@ -3,6 +3,9 @@ function placeNewNode(ev){
     hidemenu();
     let clr = ev.target.id;
     
+    console.log("Trying to place");
+    console.log(ev);
+    
     currentMolecule.placeAtom({
         x: menu.x, 
         y: menu.y, 
@@ -12,9 +15,26 @@ function placeNewNode(ev){
     }, null, availableTypes); //null indicates that there is nothing to load from the molecule list for this one
 }
 
+function placeGitHubMolecule(ev){
+    hidemenu();
+    let clr = ev.target.id;
+    
+    console.log("Trying to place");
+    console.log(clr);
+    
+    currentMolecule.placeAtom({
+        x: menu.x, 
+        y: menu.y, 
+        parent: currentMolecule,
+        atomType: "GitHubMolecule",
+        projectID: clr,
+        uniqueID: generateUniqueID()
+    }, null, availableTypes); //null indicates that there is nothing to load from the molecule list for this one
+}
+
 function showmenu(ev){
     //Open the default tab
-    document.getElementById("defaultTab").click();
+    document.getElementById("localTab").click();
     
     //stop the real right click menu
     ev.preventDefault(); 
@@ -44,28 +64,70 @@ function hidemenu(ev){
 
 function searchMenu(evt) {
   
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('menuInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("menuList");
-    li = ul.getElementsByTagName('li');
-    
-    console.log(li);
+    if(document.getElementsByClassName("tablinks active")[0].id == "localTab"){
+        console.log("local tab recognized");
+        // Declare variables
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('menuInput');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("menuList");
+        li = ul.getElementsByTagName('li');
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i]; //this is the link part of the list item
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) { //if the entered string matches
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i]; //this is the link part of the list item
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) { //if the entered string matches
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+            
+            //If enter was just pressed "click" the first element that is being displayed
+            if(evt.code == "Enter" && li[i].style.display != "none"){
+                li[i].click();
+                return;
+            }
         }
-        
-        //If enter was just pressed "click" the first element that is being displayed
-        if(evt.code == "Enter" && li[i].style.display != "none"){
-            li[i].click();
+    }
+    else{
+        console.log("github tab recognized");
+        if(evt.code == "Enter"){
+            input = document.getElementById('menuInput').value;
+            console.log("would search github now for: " + input);
+            
+            githubList = document.getElementById("githubList");
+            
+            oldResults = document.getElementsByClassName("menu-item");
+            for (i = 0; i < oldResults.length; i++) {
+                oldResults[i].style.display = "none";
+            }
+            
+            octokit.search.repos({
+                q: input,
+                sort: "stars",
+                per_page: 100,
+                topic: "maslowcreate-molecule",
+                page: 1,
+                headers: {
+                    accept: 'application/vnd.github.mercy-preview+json'
+                }
+            }).then(result => {
+                result.data.items.forEach(item => {
+                    console.log(item);
+                    if(item.topics.includes("maslowcreate-molecule")){
+                    
+                        var newElement = document.createElement("LI");
+                        var text = document.createTextNode(item.name);
+                        newElement.setAttribute("class", "menu-item");
+                        newElement.setAttribute("id", item.id);
+                        newElement.appendChild(text); 
+                        githubList.appendChild(newElement); 
+                        
+                        document.getElementById(item.id).addEventListener('click', placeGitHubMolecule);
+                    }
+                });
+            })
         }
     }
 }
