2540bd9aa786173dac8d41450c2f602c2c44c53e
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-04-03 18:07:42 +0700

    more clean up

--- src/js/molecules/stretch.js
+++ src/js/molecules/stretch.js
@@ -10,7 +10,7 @@ export default class Stretch extends Atom {
         this.addIO("input", "geometry", this, "geometry", "");
         this.addIO("input", "x", this, "number", 1);
         this.addIO("input", "y", this, "number", 1);
-        this.addIO("input", "z", this, "number", 0);
+        this.addIO("input", "z", this, "number", 1);
         this.addIO("output", "geometry", this, "geometry", "");
         
         this.name = "Stretch";
