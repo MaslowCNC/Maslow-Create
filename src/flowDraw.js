713cbde971d001c6318f55eab65361db2eb2941c
Mo Nunez <alzatin@gmail.com>
2019-04-14 15:56:01 -0700

    slider correction

--- src/flowDraw.js
+++ src/flowDraw.js
@@ -22,8 +22,9 @@ viewer.setAttribute("style","width:"+innerWidth/2+"px");
 var url = window.location.href;
 GlobalVariables.runMode = url.includes("run"); //Check if we are using the run mode based on url
 
-//slider to set atom size >> atom size also set by screen resize 
+
 document.getElementById("myslider").addEventListener("change", event =>{
+    GlobalVariables.scale1= document.getElementById("myslider").value/80;
 });
 
 // Event Listeners
