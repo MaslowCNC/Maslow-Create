90e390c24468a66e327cc9c3f08936e8f3478954
Mo Nunez <alzatin@gmail.com>
2019-05-08 23:37:04 -0700

    Grid checkbox working

--- src/js/display.js
+++ src/js/display.js
@@ -44,15 +44,16 @@ export default class Display {
         light2.position.set(1, 1, 1)
         this.camera.add(light2)
 
+
         // Sets initial plane and mesh
         var planeGeometry = new THREE.PlaneBufferGeometry( 100, 100, 60, 60)
         var planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff} )
         planeMaterial.wireframe = true
         planeMaterial.transparent = true 
         planeMaterial.opacity = 0.2
-        var plane = new THREE.Mesh( planeGeometry, planeMaterial )
-        plane.receiveShadow = true
-        this.scene.add( plane )
+        this.plane = new THREE.Mesh( planeGeometry, planeMaterial )
+        this.plane.receiveShadow = true
+        this.scene.add( this.plane )
 
         //
         this.renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -78,13 +79,19 @@ export default class Display {
             sideBar.appendChild(gridCheck)
             gridCheck.setAttribute('type', 'checkbox')
             gridCheck.setAttribute('id', 'gridCheck')
+            gridCheck.setAttribute('checked', 'true')
             var gridCheckLabel = document.createElement('label')
             sideBar.appendChild(gridCheckLabel)
             gridCheckLabel.setAttribute('for', 'gridCheck')
             gridCheckLabel.textContent= "Grid"
 
             gridCheck.addEventListener('change', event => {
-                console.log(event.target.checked)
+                if(event.target.checked){
+                    this.scene.add( this.plane )
+                }
+                else{
+                    this.scene.remove(this.plane)
+                }
             })
             
         })
