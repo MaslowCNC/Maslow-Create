3d5e98e5c564c28d6334ac6f314b56f36edd83cd
Mo Nunez <alzatin@gmail.com>
2019-05-20 20:11:49 -0700

    Create wireframe checkbox and add wireframe

--- src/js/display.js
+++ src/js/display.js
@@ -13,6 +13,7 @@ export default class Display {
         this.scene
         this.renderer
         this.stats
+        this.threeMaterial
         this.mesh
         this.gui
         this.targetDiv = document.getElementById('viewerContext')
@@ -69,7 +70,7 @@ export default class Display {
                     sideBar.removeChild(sideBar.firstChild)
                 }
                 
-                //add the name as a title
+                //Grid display html element
                 var name = document.createElement('h1')
                 name.textContent = "3D View"
                 name.setAttribute('class','doc-title')
@@ -91,6 +92,28 @@ export default class Display {
                     }
                     else{
                         this.scene.remove(this.plane)
+                    }
+                })
+
+                //Wireframe HTML element
+
+                var wireCheck = document.createElement('input')
+                sideBar.appendChild(wireCheck)
+                wireCheck.setAttribute('type', 'checkbox')
+                wireCheck.setAttribute('id', 'wireCheck')
+                wireCheck.setAttribute('checked', 'false')
+                //wireCheck.setAttribute('style', 'display:inline-block;')
+                var wireCheckLabel = document.createElement('label')
+                sideBar.appendChild(wireCheckLabel)
+                wireCheckLabel.setAttribute('for', 'wireCheck')
+                wireCheckLabel.textContent= "Wireframe"
+
+                wireCheck.addEventListener('change', event => {
+                    if(event.target.checked){
+                        this.threeMaterial.wireframe = true
+                    }
+                    else{
+                        this.threeMaterial.wireframe = false
                     }
                 })
             }
@@ -136,12 +159,12 @@ export default class Display {
         // Build new datasets from the written data, and display them.
         this.datasets = []
         
-        let threeMaterial = new THREE.MeshStandardMaterial({
+        this.threeMaterial = new THREE.MeshStandardMaterial({
             color: 0x5f6670,
             roughness: 0.65,
-            metalness: 0.40
+            metalness: 0.40   
         })
-        
+
         const walk = (geometry) => {
             if (geometry.assembly) {
                 geometry.assembly.forEach(walk)
@@ -152,7 +175,7 @@ export default class Display {
                 for (const [[aX, aY, aZ], [bX, bY, bZ]] of segments) {
                     threejsGeometry.vertices.push(new THREE.Vector3(aX, aY, aZ), new THREE.Vector3(bX, bY, bZ))
                 }
-                dataset.mesh = new THREE.LineSegments(threejsGeometry, threeMaterial)
+                dataset.mesh = new THREE.LineSegments(threejsGeometry, this.threeMaterial)
                 this.scene.add(dataset.mesh)
                 this.datasets.push(dataset)
             } else if (geometry.threejsSolid) {
@@ -161,7 +184,7 @@ export default class Display {
                 const threejsGeometry = new THREE.BufferGeometry()
                 threejsGeometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
                 threejsGeometry.addAttribute('normal', new THREE.Float32BufferAttribute(normals, 3))
-                dataset.mesh = new THREE.Mesh(threejsGeometry, threeMaterial)
+                dataset.mesh = new THREE.Mesh(threejsGeometry, this.threeMaterial)
                 this.scene.add(dataset.mesh)
                 this.datasets.push(dataset)
             } else if (geometry.threejsSurface) {
@@ -170,7 +193,7 @@ export default class Display {
                 const threejsGeometry = new THREE.BufferGeometry()
                 threejsGeometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
                 threejsGeometry.addAttribute('normal', new THREE.Float32BufferAttribute(normals, 3))
-                dataset.mesh = new THREE.Mesh(threejsGeometry, threeMaterial)
+                dataset.mesh = new THREE.Mesh(threejsGeometry, this.threeMaterial)
                 this.scene.add(dataset.mesh)
                 this.datasets.push(dataset)
             }
