2ae2ce9cafe000b84d2c7238ce109a3a815e401f
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-08-16 15:08:48 +0300

    lint

--- src/js/globalvariables.js
+++ src/js/globalvariables.js
@@ -147,12 +147,12 @@ class GlobalVariables{
          */
         this.limitedEvaluate = math.evaluate
         math.import({
-          'import':     function () { throw new Error('Function import is disabled') },
-          'createUnit': function () { throw new Error('Function createUnit is disabled') },
-          'evaluate':   function () { throw new Error('Function evaluate is disabled') },
-          'parse':      function () { throw new Error('Function parse is disabled') },
-          'simplify':   function () { throw new Error('Function simplify is disabled') },
-          'derivative': function () { throw new Error('Function derivative is disabled') }
+            'import':     function () { throw new Error('Function import is disabled') },
+            'createUnit': function () { throw new Error('Function createUnit is disabled') },
+            'evaluate':   function () { throw new Error('Function evaluate is disabled') },
+            'parse':      function () { throw new Error('Function parse is disabled') },
+            'simplify':   function () { throw new Error('Function simplify is disabled') },
+            'derivative': function () { throw new Error('Function derivative is disabled') }
         }, { override: true })
     }
     
