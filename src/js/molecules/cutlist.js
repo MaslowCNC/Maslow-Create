fe509e1511090b00ff8ed17008d20ea47dc77133
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-08-20 07:58:29 +0300

    Add a "cutlist" atom which adds a part to the list to be cut

---
+++ src/js/molecules/cutlist.js
@@ -0,0 +1,43 @@
+import Atom from '../prototypes/atom'
+import GlobalVariables from '../globalvariables.js'
+
+/**
+ * This class creates the Add To Cutlist atom.
+ */
+export default class CutList extends Atom{
+    
+    /**
+     * The constructor function.
+     * @param {object} values An array of values passed in which will be assigned to the class as this.x
+     */ 
+    constructor(values){
+        super(values)
+        
+        this.addIO('input', 'geometry', this, 'geometry', '')
+        this.addIO('output', 'geometry', this, 'geometry', '')
+        
+        /**
+         * This atom's name
+         * @type {string}
+         */
+        this.name = 'Add To Cutlist'
+        /**
+         * This atom's type
+         * @type {string}
+         */
+        this.atomType = 'cutList'
+        
+        this.setValues(values)
+    }
+    
+    /**
+     * Add a tag to the input geometry. The substance is not changed.
+     */ 
+    updateValue(){
+        try{
+            const values = [this.findIOValue('geometry'), "cutList"+GlobalVariables.generateUniqueID()]
+            this.basicThreadValueProcessing(values, "tag")
+        }catch(err){this.setAlert(err)}
+        super.updateValue()
+    }
+}
