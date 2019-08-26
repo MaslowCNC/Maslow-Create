b94f53b42a99e8ca0522aa981302a73cb852f832
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-08-26 11:53:39 +0300

    Remove node import for woker_threads to get rid of build error

--- src/js/lib/service.js
+++ src/js/lib/service.js
@@ -6,26 +6,7 @@ import { conversation } from './conversation.js';
 // Sets up a worker with conversational interface.
 export const createService = async ({ nodeWorker, webWorker, agent }) => {
   if (isNode) {
-    console.log(`QQ/worker_threads/import`);
-    const { Worker } = await import('worker_threads');
-    console.log(`QQ/worker_threads/worker`);
-    const worker = new Worker(nodeWorker);
-    const say = (message) => worker.postMessage(message);
-    const { ask, hear } = conversation({ agent, say });
-    const stop = async () => {
-      return new Promise((resolve, reject) => {
-        worker.terminate((err, exitCode) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(exitCode);
-          }
-        });
-      });
-    };
-    console.log(`QQ/worker_threads/hear`);
-    worker.on('message', hear);
-    return { ask, stop };
+    console.log(`This version doesn't work in node`);
   } else if (isBrowser) {
     const worker = new Worker(webWorker);
     const say = (message) => worker.postMessage(message);
