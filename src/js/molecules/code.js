d4abd4dfa1e78276c13415335c0b65cbab6f539e
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-10-08 10:55:11 +0200

    stash

--- src/js/molecules/code.js
+++ src/js/molecules/code.js
@@ -49,6 +49,9 @@ export default class Code extends Atom {
                 argumentsArray[input.name] = input.value
             })
             
+            console.log("Arguments passed: ")
+            console.log(argumentsArray)
+            
             const values = [this.code, argumentsArray]
             
             this.basicThreadValueProcessing(values, "code")
