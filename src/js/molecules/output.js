69a99e2e118f8b2e8e690d1c937e2e16568b2737
alzatin <alzatin@gmail.com>
2020-02-21 15:23:36 -0500

    Update output.js

--- src/js/molecules/output.js
+++ src/js/molecules/output.js
@@ -127,7 +127,7 @@ export default class Output extends Atom {
 
         GlobalVariables.c.beginPath()
         GlobalVariables.c.textAlign = 'start'
-        GlobalVariables.c.fillText(this.name, xInPixels + radiusInPixels/2, yInPixels - radiusInPixels)
+        GlobalVariables.c.fillText(this.name, xInPixels - radiusInPixels, yInPixels - radiusInPixels*1.5)
         GlobalVariables.c.moveTo(xInPixels + radiusInPixels - radiusInPixels*2, yInPixels - this.height)
         GlobalVariables.c.lineTo(xInPixels + radiusInPixels -5, yInPixels)
         GlobalVariables.c.lineTo(xInPixels + radiusInPixels - radiusInPixels*2, yInPixels + this.height)
