ca0a239c4bd73b963548e5af1fd925b083a38ef3
alzatin <alzatin@gmail.com>
2020-04-15 20:42:08 -0400

    lint

--- src/js/prototypes/atom.js
+++ src/js/prototypes/atom.js
@@ -402,7 +402,7 @@ export default class Atom {
          
         //runs whenever a key is pressed
         if (['Delete', 'Backspace'].includes(key)){
-            event.preventDefault();
+            event.preventDefault()
             if(this.selected == true && document.getElementsByTagName('BODY')[0] == document.activeElement){
                 //If this atom is selected AND the body is active (meaning we are not typing in a text box)
                 this.deleteNode()
