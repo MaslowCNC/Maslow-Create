3c9683bef522a8a6512e2eb028d14a334470c7af
alzatin <alzatin@gmail.com>
2020-05-28 12:23:16 -0400

    difference,extrude,translate

--- src/js/molecules/translate.js
+++ src/js/molecules/translate.js
@@ -1,4 +1,5 @@
 import Atom from '../prototypes/atom'
+import GlobalVariables from '../globalvariables'
 
 /**
  * This class creates the translate atom.
@@ -32,6 +33,32 @@ export default class Translate extends Atom{
         this.setValues(values)
     }
     
+     /**
+     * Draw the translate icon.
+     */ 
+      draw(){
+
+        super.draw() //Super call to draw the rest
+        
+        GlobalVariables.c.beginPath()
+        GlobalVariables.c.fillStyle = '#949294'
+        GlobalVariables.c.arc(GlobalVariables.widthToPixels(this.x+this.radius/5), 
+            GlobalVariables.heightToPixels(this.y), 
+            GlobalVariables.widthToPixels(this.radius/2), 0, Math.PI * 2, false)       
+        //GlobalVariables.c.fill()
+        GlobalVariables.c.stroke() 
+        GlobalVariables.c.closePath()  
+        GlobalVariables.c.beginPath()
+        GlobalVariables.c.fillStyle = '#949294'
+        GlobalVariables.c.arc(GlobalVariables.widthToPixels(this.x-this.radius/5), 
+            GlobalVariables.heightToPixels(this.y), 
+            GlobalVariables.widthToPixels(this.radius/2), 0, Math.PI * 2, false)       
+        GlobalVariables.c.fill()
+        GlobalVariables.c.stroke() 
+        GlobalVariables.c.closePath()  
+          
+
+    }
     /**
      * Pass the input geometry to a worker function to compute the translation.
      */ 
