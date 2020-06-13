23f86f901876b901ac5991917d3561f3619ea7e0
alzatin <alzatin@gmail.com>
2020-06-13 16:58:30 -0400

    input if statement rewrite

--- src/flowDraw.js
+++ src/flowDraw.js
@@ -124,11 +124,13 @@ flowCanvas.addEventListener('mouseup', event => {
 * @type {array}
 */
 window.addEventListener('keydown', e => {
+    //console.log(e.srcElement.tagName.toLowerCase())
     //Prevents default behavior of the browser on canvas to allow for copy/paste/delete
-    if((e.srcElement.tagName.toLowerCase() !== ("textarea" && "input"))
+    if(e.srcElement.tagName.toLowerCase() !== ("textarea")
+        && e.srcElement.tagName.toLowerCase() !== ("input")
         &&(!e.srcElement.isContentEditable)
         && ['c','v', 'Backspace'].includes(e.key)){
-        e.preventDefault()
+            e.preventDefault()
     }
     //Copy /paste listeners
     if (e.key == "Control" || e.key == "Meta") {
