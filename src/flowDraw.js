d2faa3bd6e54d2d611c32cbfaa3b7bb941596b94
alzatin <alzatin@gmail.com>
2020-06-22 20:15:14 -0400

    click up

--- src/flowDraw.js
+++ src/flowDraw.js
@@ -112,8 +112,10 @@ flowCanvas.addEventListener('dblclick', event => {
 
 
 document.addEventListener('mouseup',(e)=>{
-    if (!e.srcElement.isContentEditable){
 
+     if(e.srcElement.tagName.toLowerCase() !== ("textarea")
+        && e.srcElement.tagName.toLowerCase() !== ("input")
+        &&(!e.srcElement.isContentEditable)){
         //puts focus back into mainbody after clicking button
         document.activeElement.blur()
         document.getElementById("mainBody").focus()
