481ee12a937a97c37b28ceb86d61801f9f597f90
BarbourSmith <BarbourSmith@users.noreply.github.com>
2020-08-14 11:16:17 -0700

    Gets us to like -6

--- src/js/molecules/molecule.js
+++ src/js/molecules/molecule.js
@@ -183,7 +183,6 @@ export default class Molecule extends Atom{
         //Tell the correct input to update
         this.nodesOnTheScreen.forEach(atom => { //Scan all the input atoms
             if(atom.atomType == 'Input' && atom.name == targetName){  //When there is a match
-                console.log("Passing information to: " + targetName)
                 this.processing = true  //Sets to true processing variable??
                 atom.updateValue() //Tell that input to update it's value
             }
