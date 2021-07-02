e758bc76090d569d7dccff462b67d1672dbb32be
BarbourSmith <BarbourSmith@users.noreply.github.com>
2021-07-02 18:22:06 -0500

    Fix has sides

--- dist/maslowWorker.js
+++ dist/maslowWorker.js
@@ -149,7 +149,7 @@ const agent = async ({
             break;
           case "circle":
             console.log(api.Arc(10));
-            const aCircle = api.Arc(question.diameter);//.sides(question.numSegments);
+            const aCircle = api.Arc(question.diameter).hasSides(question.numSegments);
             await api.saveGeometry(question.writePath, aCircle);
             return 1;
             break;
